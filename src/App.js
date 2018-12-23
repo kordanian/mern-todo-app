c7cdc9c6f716e482b5f07a75c7ab8d964f0bee7d
Zenji <jayterry7@gmail.com>
2018-12-22 20:02:53 -0700

    add mern todo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import CreateToDo from './components/create-todo.component';
+import EditToDo from './components/edit-todo.component';
+import ToDoList from './components/todos-list.component';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="container">
+          <h1>Mern stack todo app</h1>
+          <Route path="/" exact component={ToDoList} />
+          <Route path="/edit/:id" component={EditToDo} />
+          <Route path="/create" component={CreateToDo} />
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/components/create-todo.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class CreateToDo extends Component {
+    render() {
+        return (
+            <div>
+                <p>Welcome to Create ToDo</p>
+            </div>
+        )
+    }
+}
