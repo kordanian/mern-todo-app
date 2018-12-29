161ccfa951d8a7e09363ed80180bfcdd7e299bfd
Zenji <jay@jtpro.net>
2018-12-29 15:17:36 -0700

    updated to axios and alert logic

--- src/components/todos-list.component.js
+++ src/components/todos-list.component.js
@@ -1,33 +1,52 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
+const customStyles = {
+    content : {
+      top                   : '50%',
+      left                  : '50%',
+      right                 : 'auto',
+      bottom                : 'auto',
+      marginRight           : '-50%',
+      transform             : 'translate(-50%, -50%)'
+    }
+  };
+
 export default class ToDosList extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            data: []
+            data: [],
+            modalIsOpen: false
         };
         fetch('http://localhost:4000/todos')
             .then(response => response.json())
             .then(data => {
                 this.setState({ data });
-            });  
-    }
+            });
+    } 
 
-    handleClick = (e) => {
+    handleClick = e => {
         e.preventDefault();
-        console.log('this is:', e);
+        let that = this;
         axios.post(`http://localhost:4000/todos/delete/${e.currentTarget.id}`)
             .then(data => {
-                console.log(data);
-                window.location.reload();
+                if (data.status === 200) {
+                    window.location.reload();
+                } else {
+                    window.alert(`status ${data.status}`);
+                    window.location.reload();
+                }
+            })
+            .catch(error => {
+                window.alert(error);
             })
     }
 
     render() {
         return (
             <div>
-                <p>Welcome to ToDos List</p>
+                  <p>Welcome to ToDos List</p>
                 <ul>
                     {
                         this.state.data.map((item, i) => {
